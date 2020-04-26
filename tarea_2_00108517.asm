18263bf3ee90eace4cebad77f85ac8a7482edc03
RickyGomez10 <00108517@uca.edu.sv>
2020-04-25 23:19:08 -0600

    Ejercicio 2 - grafica de nayib dinamica

--- tarea_2_00108517.asm
+++ tarea_2_00108517.asm
@@ -37,4 +37,25 @@
         int 20h
  
       
-       
+       ;ejercicio 2
+        mov al, 2d
+        mov bl, 2d
+        mov bx, 2d
+        mov cx, 11d
+        mov di, 0d
+        ciclo:
+            mul bl
+            jo foverflow
+            mov [di+200h], al
+            add di, 1d
+            loop ciclo
+            int 20h
+            
+        foverflow:
+            mov [di+200h], al
+            add di, 1
+            mov [di+200h], ah
+            add di, 1
+            mul bx
+            loop fcarry
+            int 20h
