86a7e331f1a47ef1aa5eb9a80e171b54f0fc09b6
RickyGomez10 <00108517@uca.edu.sv>
2020-04-25 22:48:04 -0600

    Ejercicio 1 - Respuesta quemada

---
+++ tarea_2_00108517.asm
@@ -0,0 +1,40 @@
+        ;Ejercicio 1
+        mov ax, 0
+        mov bx, 8
+        add ax,bx
+        mov bx, 5
+        add ax, bx
+        mov bx, 1
+        add ax, bx
+        mov bx, 7
+        add ax, bx
+        mov bx, 5
+        div bx
+        
+        mov cl, "M"
+        mov  [200h], cl
+        mov cl, "E"
+        mov  [201h], cl
+        mov cl, " "
+        mov  [202h], cl
+        mov cl, "R"
+        mov  [203h], cl
+        mov cl, "E"
+        mov  [204h], cl
+        mov cl, "C"
+        mov  [205h], cl
+        mov cl, "U"
+        mov  [206h], cl
+        mov cl, "P"
+        mov  [207h], cl
+        mov cl, "E"
+        mov  [208h], cl
+        mov cl, "R"
+        mov  [209h], cl
+        mov cl, "0"
+        mov  [20Ah], cl
+        
+        int 20h
+ 
+      
+       
