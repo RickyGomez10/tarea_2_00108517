580044e25908364a43162de44a78b3605b703150
RickyGomez10 <00108517@uca.edu.sv>
2020-04-25 23:21:47 -0600

    Ejercicio 3 - fibonacci dinamico

--- tarea_2_00108517.asm
+++ tarea_2_00108517.asm
@@ -59,3 +59,26 @@
             mul bx
             loop fcarry
             int 20h
+
+        ;Ejercicio 3
+        mov ax, 0d 
+        mov bx, 1d 
+        mov cx, 12d 
+        mov di, 0d 
+        mov [di+200h], ax 
+        add di,1 
+        mov[di+200h],bx 
+        add ax,bx 
+        add di,1 
+        mov[di+200h], ax 
+        add ax,bx 
+        add di, 1 
+        fibonacci:
+            mov[di + 200h], ax 
+            mov dx, ax
+            add ax, bx
+            mov bx, dx
+            add di, 1d
+            loop fibonacci
+            int 20h    
+    
